3f4d471f3232f074c529eacdf931ce14f148753f
Zuzana Kormancova <zunakorma@gmail.com>
2022-03-31 19:18:01 +0200

    test AWS Elastic Beanstalk with Pipeline

--- run.py
+++ run.py
@@ -2,6 +2,10 @@ from flaskr import create_app
 
 app = create_app()
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080,debug=True)
+# if __name__ == "__main__":
+#     app.run(host='0.0.0.0', port=8080,debug=True)
 
+# for AWS Elastic Beanstalk => application!
+application=app
+if __name__ == "__main__":
+    application.run(host='0.0.0.0', port=8080,debug=True)
